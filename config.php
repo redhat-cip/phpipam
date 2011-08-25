280d835f753d18f942e064c8345b11e6773e77c4
myha <myha@d9489115-519a-4661-88eb-0d2216f09706>
2011-08-25 07:27:16 +0000

    Fist copy
    git-svn-id: svn://svn.code.sf.net/p/phpipam/code@1 d9489115-519a-4661-88eb-0d2216f09706

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+/*	database connection details
+ ******************************/
+$db['host'] = "localhost";
+$db['user'] = "ipv6";
+$db['pass'] = "ipv6admin";
+$db['name'] = "phpipam";
+
+/**
+ * php debugging on/off
+ *
+ * 1 = SHOW all php errors
+ * 0 = HIDE all php errors
+ ******************************/
+$debugging = 0;
+
+?>
