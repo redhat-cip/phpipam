1148287aea72ee23e8303304395a23964c47ceca
myha <myha@d9489115-519a-4661-88eb-0d2216f09706>
2012-02-16 12:44:12 +0000

    Added UPDATE sql file for v6
    git-svn-id: svn://svn.code.sf.net/p/phpipam/code@88 d9489115-519a-4661-88eb-0d2216f09706

---
+++ db/UPDATE-v0.5.sql
@@ -0,0 +1,7 @@
+/************************
+Update from v 0.5 to 0.6 
+************************/
+
+/* UPDATE version */
+UPDATE `settings` set `version` = '0.6';
+UPDATE `settings` set `donate` = '0';
