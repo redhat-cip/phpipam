8e4f78c603d2225ba24d7b358b0a0ccc9e0fb14f
myha <myha@d9489115-519a-4661-88eb-0d2216f09706>
2012-03-20 08:47:36 +0000

    Fixed wrong versions on new installs
    git-svn-id: svn://svn.code.sf.net/p/phpipam/code@180 d9489115-519a-4661-88eb-0d2216f09706

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -35,7 +35,7 @@ LOCK TABLES `settings` WRITE;
 
 INSERT INTO `settings` (`id`, `siteTitle`, `siteAdminName`, `siteAdminMail`, `siteDomain`, `siteURL`, `domainAuth`, `showTooltips`, `enableIPrequests`, `enableDNSresolving`, `version`, `IPfilter`)
 VALUES
-	(1,'phpipam IP address management','Sysadmin','admin@domain.local','domain.local','yourpublicurl.com',0,1,1,0, '0.5', 'mac;owner;state;switch;port;note');
+	(1,'phpipam IP address management','Sysadmin','admin@domain.local','domain.local','yourpublicurl.com',0,1,1,0, '0.6', 'mac;owner;state;switch;port;note');
 
 /*!40000 ALTER TABLE `settings` ENABLE KEYS */;
 UNLOCK TABLES;
