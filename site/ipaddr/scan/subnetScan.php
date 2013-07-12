e04d9b14eb0d5cbc36ffb9fd4f001e4c5aa0cae9
Julien Parent-Trudeau <julien.parent.trudeau@enovance.com>
2013-07-12 11:34:48 -0400

    Disabled non implemented subnet scan tools (Nmap, SNMP)

--- site/ipaddr/scan/subnetScan.php
+++ site/ipaddr/scan/subnetScan.php
@@ -48,13 +48,13 @@ $ip_addr = getIpAddressesBySubnetId ($_POST['subnetId']) ;
     			<!-- Discovery scans -->
 	    		<optgroup label="<?php print _('Discovery scans');?>">
 		    		<option value="DiscoveryPing">Ping <?php print _('scan');?></option>
-		    		<option value="DiscoveryNmap">NMap <?php print _('scan');?></option>
-		    		<option value="DiscoverySnmp">SNMP <?php print _('scan');?></option>
+		    		<option value="DiscoveryNmap" disabled="disabled">NMap <?php print _('scan');?></option>
+		    		<option value="DiscoverySnmp" disabled="disabled">SNMP <?php print _('scan');?></option>
 	    		</optgroup>
     			<!-- Status update scans -->
 	    		<optgroup label="<?php print _('Status update scans');?>">
 		    		<option value="UpdatePing">Ping <?php print _('scan');?></option>
-		    		<option value="UpdateNmap">NMap <?php print _('scan');?></option>
+		    		<option value="UpdateNmap" disabled="disabled">NMap <?php print _('scan');?></option>
 	    		</optgroup>
 
 			</select>
